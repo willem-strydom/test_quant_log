9a5fe23222c0b8f8e1bc40106c83f563962c3a40
willem-strydom <s.willem@wustl.edu>
2023-12-18 10:27:58 -0900

    troubleshooting

--- binning.py
+++ binning.py
@@ -8,15 +8,13 @@ def binning(vals, bins: int, type:str, scale):
 
     :param type: "unif" or "gauss" -> assumed distribution
     :param bins: log2 number of bins... must be 1,2,3,or 4 for now
-    :param var: variance of values
-    :param mean: mean of values
+    :param scale: either 2**1/2 var
     :return: (2^num_bins)+1 partitions.. need to chop the ends off though numpy 1xd array
     """
     var = np.var(vals)
     mean = np.mean(vals)
     if scale == "sqrd":
-        scale = mean
-    # if statements...nice
+        scale = var
     if type == 'unif':
         a = mean - np.sqrt(3*var)
         b = mean + np.sqrt(3*var)
@@ -32,3 +30,14 @@ def binning(vals, bins: int, type:str, scale):
             return np.arange(-2.68,2.69,0.335) * var * scale + mean
 
 
+def _binning():
+    vals = np.arange(0,11,1)
+    bins = [1,2,3,4]
+    types = ['unif', 'gauss']
+    scales = [np.sqrt(2), 1/2, np.mean(vals)]
+    for bin in bins:
+        for type in types:
+            for scale in scales:
+                print(binning(vals,bin,type,scale), f"{bin}, {type}, {scale}")
+
+_binning()
